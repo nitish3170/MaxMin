a9ebdf641b3f8cb01fb582118d2f64caa2715ae9
david <david@Nitishs-MacBook-Air-501.local>
2024-07-01 11:08:13 +0530

    Print using generic methode

--- src/MaxMin.java
+++ src/MaxMin.java
@@ -1,38 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class MaxMin <T extends Comparable<T>>{
-    private T a;
-    private T b;
-    private T c;
-    public MaxMin(T a, T b,T c){
-        this.a=a;
-        this.b=b;
-        this.c=c;
+    private List<T> values;
+    public MaxMin(T... values){
+        this.values = Arrays.asList(values);
     }
-    public static<T extends Comparable<T>> void maxMin(T a , T b, T c){
-        T max = a.compareTo(b) > 0 ? a : b;
-        max = max.compareTo(c) > 0 ? max : c;
-        System.out.println("Max of given values is: " + max);
+    public static <T extends Comparable<T>> void maxMin(List<T> values) {
+        System.out.println(Collections.max(values));
     }
-    public void maxMin() {
-        maxMin(a,b,c);
-    }
-    @SafeVarargs
-    public static <T extends Comparable<T>> T maxMin(T... values) {
-        Arrays.sort(values);
-        return values[values.length - 1];
+    public void printMax(){
+        maxMin(this.values);
     }
 
     public static void main(String[] args) {
-        MaxMin<Integer>int1=new MaxMin<>(12,4,5);
-        maxMin(12,33,56,78,33);
-        MaxMin<Float>float1=new MaxMin<>(12.1f,4.1f,5.1f);
-        maxMin(2.1f,34.1f,15.1f);
-        MaxMin<String>string1=new MaxMin<>("Apple","Peach","Banana");
-        maxMin("Apple","Peach","Banana","Grapes");
-        int1.maxMin();
-        float1.maxMin();
-        string1.maxMin();
+        MaxMin<Integer>int1=new MaxMin<>(12,33,56,78,33);
+        MaxMin<Float>float1=new MaxMin<>(2.1f,34.1f,15.1f);
+        MaxMin<String>string1=new MaxMin<>("Apple","Peach","Banana","Grapes");
+        int1.printMax();
+        float1.printMax();
+        string1.printMax();
 
     }
 }
